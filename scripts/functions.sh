a53c9616b5745827e772ce9f7c34e5e2703a8e48
deyangeor <52912071+deyangeor@users.noreply.github.com>
2020-07-16 18:46:26 +0300

    Update functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -11,22 +11,20 @@ function download {
         echo "Downloading $file from $url has failed"
         exit $retval
     fi
-
     echo "Download finished successfully"
-
 }
 
 function get_binary {
-   name=$1
-   url=$2
-   bin_dir="$3"
+    name=$1
+    url=$2
+    bin_dir="$3"
 
-   if [[ ! -d $bin_dir ]]; then
-       mkdir -p $bin_dir
-       export PATH="${PATH}:${bin_dir}"
-   fi
+    if [[ ! -d $bin_dir ]]; then
+        mkdir -p $bin_dir
+        export PATH="${PATH}:${bin_dir}"
+    fi
 
-   download $url "${bin_dir}/${name}"
-   chmod 755 ${bin_dir}/${name}
-   sleep 1
+    download $url "${bin_dir}/${name}"
+    chmod 755 ${bin_dir}/${name}
+    sleep 1
 }
