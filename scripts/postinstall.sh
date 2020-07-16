7a9e98c6e13fd8db22b07cc2a6d19acd276490cd
Your Name <you@example.com>
2020-07-16 16:39:05 +0300

    stop being an idiot

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,8 +2,8 @@
 
 source /tmp/provisioning/config.sh
 
-echo "Cleaning up ..."
-rm -rf /tmp/provisioning
-
 echo -e "Useful information\n\n"
 cat $OUTPUT_FILE
+
+echo -e "\n\nCleaning up ..."
+rm -rvf /tmp/provisioning
