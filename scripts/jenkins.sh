86a3057c3247c8cff7e2e43746cd1ffcc3bc2a82
deyangeor <52912071+deyangeor@users.noreply.github.com>
2020-07-16 18:44:26 +0300

    Update jenkins.sh

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -3,7 +3,7 @@
 source /tmp/provisioning/functions.sh
 source /tmp/provisioning/config.sh
 
-curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+download http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo /etc/yum.repos.d/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 
 yum clean all
@@ -20,16 +20,16 @@ sleep 60
 JENKINS_PASS=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 
 if [[ ! -d $JENKINS_CLI_DIR ]]; then
-	mkdir -p $JENKINS_CLI_DIR
+    mkdir -p $JENKINS_CLI_DIR
 fi
 
 get_binary jenkins-cli.jar http://localhost:8080/jnlpJars/jenkins-cli.jar $JENKINS_CLI_DIR
 
 for plugin in $JENKINS_PLUGINS; do
-	java -jar $JENKINS_CLI_JAR \
-	-s http://localhost:8080/ \
-	-auth $JENKINS_USER:$JENKINS_PASS \
-	install-plugin $plugin
+    java -jar $JENKINS_CLI_JAR \
+    -s http://localhost:8080/ \
+    -auth $JENKINS_USER:$JENKINS_PASS \
+    install-plugin $plugin
 done
 
 echo ":: initial admin password ::" >> $OUTPUT_FILE
